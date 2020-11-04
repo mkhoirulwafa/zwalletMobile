f9f7b5eb72945e52c29d7d6e7dfb3d619e52127a
mkhoirulwafa <masapaapa@gmail.com>
2020-11-05 01:57:50 +0700

    page login done

--- App.js
+++ App.js
@@ -14,11 +14,13 @@ import {RectButton, TouchableOpacity} from 'react-native-gesture-handler';
 import {Button, IconButton} from 'react-native-paper';
 
 const App = (props) => {
-  const {inputPassword} = useRef();
-  const {email, setEmail} = useState(null);
-  const {password, setPassword} = useState(null);
+  const inputPassword = useRef();
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
   const [loading, setLoading] = useState(false);
   const [hidePassword, setHidePassword] = useState(true);
+  const [emailFocus, setEmailFocus] = useState(true);
+  const [passwordFocus, setPasswordFocus] = useState(true);
 
   const onSubmit = () => {
     setLoading(true);
@@ -43,7 +45,7 @@ const App = (props) => {
           </View>
           <View style={styles.child}>
             <View style={styles.wrapContent}>
-              <ScrollView>
+              <View>
                 <View>
                   <Text style={styles.childTitle}>Login</Text>
                   <Text style={styles.descript}>
@@ -54,7 +56,10 @@ const App = (props) => {
                 <View style={styles.inputItem}>
                   <View style={{flex: 1}}>
                     <TextInput
-                      inlineImageLeft="mail"
+                      underlineColorAndroid={emailFocus ? '#a9a9a9' : '#6379F4'}
+                      inlineImageLeft={emailFocus ? 'mail' : 'mail_blue'}
+                      onFocus={() => setEmailFocus(!emailFocus)}
+                      onBlur={() => setEmailFocus(!emailFocus)}
                       inlineImagePadding={40}
                       placeholder="Enter your email"
                       autoCapitalize={'none'}
@@ -68,7 +73,12 @@ const App = (props) => {
                 <View style={styles.inputItem}>
                   <View style={{flex: 0.9}}>
                     <TextInput
-                      inlineImageLeft="lock"
+                      underlineColorAndroid={
+                        passwordFocus ? '#a9a9a9' : '#6379F4'
+                      }
+                      inlineImageLeft={passwordFocus ? 'lock' : 'lock_blue'}
+                      onFocus={() => setPasswordFocus(!passwordFocus)}
+                      onBlur={() => setPasswordFocus(!passwordFocus)}
                       inlineImagePadding={40}
                       ref={inputPassword}
                       value={password}
@@ -82,7 +92,10 @@ const App = (props) => {
                   </View>
                   <View style={{flex: 0.1}}>
                     <IconButton
-                      icon={hidePassword ? 'eye-outline' : 'eye-off-outline'}
+                      underlineColorAndroid={
+                        passwordFocus ? '#a9a9a9' : '#6379F4'
+                      }
+                      icon={hidePassword ? 'eye-off-outline' : 'eye-outline'}
                       color="#a9a9a9"
                       onPress={() => setHidePassword(!hidePassword)}
                     />
@@ -102,19 +115,21 @@ const App = (props) => {
                   disabled={loading}
                   loading={loading}
                   style={styles.button}>
-                  Login
+                  <Text style={{color: '#fff'}}>Login</Text>
                 </Button>
-                <Text style={styles.descript}>
-                  Don't have an account? Let's{'   '}
+                <Text style={[styles.descript]}>
+                  Don't have an account? Let's{'  '}
                   <RectButton
                     style={styles.linkWrap}
                     onPress={() => ToastAndroid.show('oii')}>
                     <View>
-                      <Text style={styles.link}>Signup</Text>
+                      <Text style={[styles.link, {color: '#6379F4'}]}>
+                        Signup
+                      </Text>
                     </View>
                   </RectButton>
                 </Text>
-              </ScrollView>
+              </View>
             </View>
           </View>
         </ScrollView>
@@ -125,11 +140,12 @@ const App = (props) => {
 
 const styles = StyleSheet.create({
   scrollView: {
-    backgroundColor: '#E5E5E5',
-    minHeight: '100%',
+    // backgroundColor: '#E5E5E5',
+    // minHeight: '10%',
+    overflow: 'hidden',
   },
   title: {
-    fontFamily: 'NunitoSans-ExtraBold',
+    fontFamily: 'NunitoSans-Bold',
     fontWeight: '700',
     fontSize: 26,
     paddingVertical: Dimensions.get('screen').height - 670,
@@ -141,6 +157,8 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 40,
     borderTopRightRadius: 40,
     padding: 20,
+    minHeight:
+      Dimensions.get('screen').height - (Dimensions.get('screen').height - 670),
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
@@ -163,16 +181,30 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#a9a9a9',
     lineHeight: 23,
-    marginBottom: 10,
+    marginBottom: 15,
   },
   inputItem: {
+    flexDirection: 'row',
     padding: 5,
-    borderColor: '#a9a9a9',
     marginVertical: 5,
     borderRadius: 5,
-    borderBottomWidth: 1,
+    // borderBottomWidth: 1,
+    marginBottom: 15,
   },
-  link: {color: '#a9a9a9', fontWeight: '700'},
+  link: {
+    color: '#a9a9a9',
+    fontWeight: '700',
+    textAlign: 'right',
+  },
+  wrapContent: {
+    flexDirection: 'column',
+    flex: 1,
+  },
+  button: {
+    marginVertical: 30,
+    padding: 5,
+    borderRadius: 15,
+  },
 });
 
 export default App;
